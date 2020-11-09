68f2056b79a381fd67db1a9a1bb515150470d871
Dominic <ngocatp.52@gmail.com>
2020-11-09 09:27:50 +0700

    Update 9/11/2020

--- src/main/java/Entity/User.java
+++ src/main/java/Entity/User.java
@@ -10,38 +10,67 @@ package Entity;
  * @author Ngoc Do Minh
  */
 public class User {
-    private String userEmail;
-    private String userName;
-    private int userRole;
+    private String accountEmail;
+    private String _username;
+    private String _password;
+    private String userFullName;
     private String userAddress;
+    private String _role;
+    private String _status;
+    private String userDoB;
     private String userAvatar;
-    private String userAddedDate;
+    private String userGender;
+    private String userDescription;
+    private String dateAdded; 
 
     public User() {
     }
 
-    public String getUserEmail() {
-        return userEmail;
+    public User(String accountEmail, String _username, String _password, String userFullName, String userAddress, String _role, String _status, String userDoB, String userAvatar, String userGender, String userDescription, String dateAdded) {
+        this.accountEmail = accountEmail;
+        this._username = _username;
+        this._password = _password;
+        this.userFullName = userFullName;
+        this.userAddress = userAddress;
+        this._role = _role;
+        this._status = _status;
+        this.userDoB = userDoB;
+        this.userAvatar = userAvatar;
+        this.userGender = userGender;
+        this.userDescription = userDescription;
+        this.dateAdded = dateAdded;
     }
 
-    public void setUserEmail(String userEmail) {
-        this.userEmail = userEmail;
+    public String getAccountEmail() {
+        return accountEmail;
     }
 
-    public String getUserName() {
-        return userName;
+    public void setAccountEmail(String accountEmail) {
+        this.accountEmail = accountEmail;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
+    public String getUsername() {
+        return _username;
     }
 
-    public int getUserRole() {
-        return userRole;
+    public void setUsername(String _username) {
+        this._username = _username;
     }
 
-    public void setUserRole(int userRole) {
-        this.userRole = userRole;
+    public String getPassword() {
+        return _password;
+    }
+
+    public void setPassword(String _password) {
+        this._password = _password;
+    }
+
+    public String getUserFullName() {
+        return userFullName;
+    }
+
+    public void setUserFullName(String userFullName) {
+        this.userFullName = userFullName;
     }
 
     public String getUserAddress() {
@@ -52,6 +81,30 @@ public class User {
         this.userAddress = userAddress;
     }
 
+    public String getRole() {
+        return _role;
+    }
+
+    public void setRole(String _role) {
+        this._role = _role;
+    }
+
+    public String getStatus() {
+        return _status;
+    }
+
+    public void setStatus(String _status) {
+        this._status = _status;
+    }
+
+    public String getUserDoB() {
+        return userDoB;
+    }
+
+    public void setUserDoB(String userDoB) {
+        this.userDoB = userDoB;
+    }
+
     public String getUserAvatar() {
         return userAvatar;
     }
@@ -60,11 +113,28 @@ public class User {
         this.userAvatar = userAvatar;
     }
 
-    public String getUserAddedDate() {
-        return userAddedDate;
+    public String getUserGender() {
+        return userGender;
     }
 
-    public void setUserAddedDate(String userAddedDate) {
-        this.userAddedDate = userAddedDate;
-    }    
+    public void setUserGender(String userGender) {
+        this.userGender = userGender;
+    }
+
+    public String getUserDescription() {
+        return userDescription;
+    }
+
+    public void setUserDescription(String userDescription) {
+        this.userDescription = userDescription;
+    }
+
+    public String getDateAdded() {
+        return dateAdded;
+    }
+
+    public void setDateAdded(String dateAdded) {
+        this.dateAdded = dateAdded;
+    }
+    
 }
---
+++ src/main/java/Model/UserDataProcess.java
@@ -0,0 +1,170 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import Entity.User;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Ngoc Do Minh
+ */
+public class UserDataProcess {
+
+    public Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            String url = "jdbc:sqlserver://127.0.0.1:1433;DatabaseName=BTEC4rumDB";
+            String username = "sa";
+            String pass = "anhyeuem";
+            try {
+                connection = DriverManager.getConnection(url, username, pass);
+            } catch (SQLException ex) {
+                Logger.getLogger(UserDataProcess.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(UserDataProcess.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return connection;
+    }
+
+    public List<User> getData() {
+        List<User> listUser = new ArrayList<>();
+        String sqlQuery = "SELECT * FROM tblAccount WHERE _role > 1";
+        try {
+            PreparedStatement preparedStatement = getConnection().prepareStatement(sqlQuery);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                User user = new User();
+                user.setAccountEmail(resultSet.getString(1));
+                user.setUsername(resultSet.getString(2));
+                user.setPassword(resultSet.getString(3));
+                user.setUserFullName(resultSet.getString(4));
+                user.setUserAddress(resultSet.getString(5));
+                user.setRole(resultSet.getString(6));
+                user.setStatus(resultSet.getString(7));
+                user.setUserDoB(resultSet.getString(8));
+                user.setUserAvatar(resultSet.getString(9));
+                user.setUserGender(resultSet.getString(10));
+                user.setUserDescription(resultSet.getString(11));
+                user.setDateAdded(resultSet.getString(12));
+                listUser.add(user);
+            }
+            resultSet.close();
+            preparedStatement.close();
+            getConnection().close();
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDataProcess.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return listUser;
+    }
+
+    public List<User> searchUserByName(String searchContent) {
+        List<User> listUser = new ArrayList<>();
+        String sqlQuery = "SELECT * FROM tblAccount WHERE userFullName LIKE '%?%'";
+        try {
+            PreparedStatement preparedStatement = getConnection().prepareStatement(sqlQuery);
+            preparedStatement.setString(1, searchContent);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                User user = new User();
+                user.setAccountEmail(resultSet.getString(1));
+                user.setUsername(resultSet.getString(2));
+                user.setPassword(resultSet.getString(3));
+                user.setUserFullName(resultSet.getString(4));
+                user.setUserAddress(resultSet.getString(5));
+                user.setRole(resultSet.getString(6));
+                user.setStatus(resultSet.getString(7));
+                user.setUserDoB(resultSet.getString(8));
+                user.setUserAvatar(resultSet.getString(9));
+                user.setUserGender(resultSet.getString(10));
+                user.setUserDescription(resultSet.getString(11));
+                user.setDateAdded(resultSet.getString(12));
+                listUser.add(user);
+            }
+            resultSet.close();
+            preparedStatement.close();
+            getConnection().close();
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDataProcess.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return listUser;
+    }
+    
+    public boolean addUser(String accountEmail, String _username, String _password, String userFullname, String userAddress, int _role, int _status, String userDoB, String userAvatar, int userGender, String userDescription) {
+        boolean isAdded = false;
+        String sqlQuery = "INSERT INTO tblAccount VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, CURRENT_TIMESTAMP)";
+        try {
+            PreparedStatement preparedStatement = getConnection().prepareStatement(sqlQuery);
+            preparedStatement.setString(1, accountEmail);
+            preparedStatement.setString(2, _username);
+            preparedStatement.setString(3, _password);
+            preparedStatement.setString(4, userFullname);
+            preparedStatement.setString(5, userAddress);
+            preparedStatement.setInt(6, _role);
+            preparedStatement.setInt(7, _status);
+            preparedStatement.setString(8, userDoB);
+            preparedStatement.setString(9, userAvatar);
+            preparedStatement.setInt(10, userGender);
+            preparedStatement.setString(11, userDescription);
+            isAdded = (preparedStatement.executeUpdate() > 0);
+            preparedStatement.close();
+            getConnection().close();
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDataProcess.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return isAdded;
+    }
+
+    public boolean updateUser(String accountEmail, String _username, String _password, String userFullname, String userAddress, int _role, int _status, String userDoB, String userAvatar, int userGender, String userDescription) {
+        boolean isUpdate = false;
+        String sqlQuery = "UPDATE tblAccount SET _username = ?, _password = ?, userFullName = ?, userAddress = ?, _role = ?, _status = ?,  userDoB = ?, userAvatar = ?, userGender = ?, userDescription = ? WHERE accountEmail = ?";
+        try {
+            PreparedStatement preparedStatement = getConnection().prepareStatement(sqlQuery);
+            preparedStatement.setString(1, _username);
+            preparedStatement.setString(2, _password);
+            preparedStatement.setString(3, userFullname);
+            preparedStatement.setString(4, userAddress);
+            preparedStatement.setInt(5, _role);
+            preparedStatement.setInt(6, _status);
+            preparedStatement.setString(7, userDoB);
+            preparedStatement.setString(8, userAvatar);
+            preparedStatement.setInt(9, userGender);
+            preparedStatement.setString(10, userDescription);
+            preparedStatement.setString(11, accountEmail);
+            isUpdate = (preparedStatement.executeUpdate() > 0);
+            preparedStatement.close();
+            getConnection().close();
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDataProcess.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return isUpdate;
+    }
+
+    public boolean deleteUser(String accountEmail) {
+        boolean isDelete = false;
+        String sqlQuery = "DELETE tblAccount WHERE accountEmail = ?";
+        try {
+            PreparedStatement preparedStatement = getConnection().prepareStatement(sqlQuery);
+            preparedStatement.setString(1, accountEmail);
+            isDelete = (preparedStatement.executeUpdate() > 0);
+            preparedStatement.close();
+            getConnection().close();
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDataProcess.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return isDelete;
+    }
+}
